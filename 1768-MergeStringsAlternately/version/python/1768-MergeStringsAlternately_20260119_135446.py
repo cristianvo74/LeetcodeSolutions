4c5aa022d4702c5ba1616591dcb8a0b17752c5d1
Cristian Andrés Villarreal Orozco <147870401+cristianvo74@users.noreply.github.com>
2026-01-19 13:54:47 -0500

    Create 1768-MergeStringsAlternately

---
+++ 1768-MergeStringsAlternately/version/python/1768-MergeStringsAlternately_20260119_135446.py
@@ -0,0 +1,23 @@
+# Last updated: 19/1/2026, 1:54:46 p. m.
+# This solution only iterates the min length word
+1class Solution:
+2    def mergeAlternately(self, word1: str, word2: str) -> str:
+3        len_1 = len(word1)
+4        len_2 = len(word2)
+5
+6        if len_1 <= len_2:
+7            min_l = len_1
+8            max_w = word2
+9        else:
+10            min_l = len_2
+11            max_w = word1
+12
+13        new_w = ""
+14        for i in range(min_l):
+15            a = word1[i]
+16            b = word2[i]
+17            new_w += a
+18            new_w += b
+19        new_w += max_w[i+1:]
+20
+21        return new_w
