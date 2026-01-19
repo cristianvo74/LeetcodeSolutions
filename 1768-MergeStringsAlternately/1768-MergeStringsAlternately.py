dab87ccafa5b5eb4b9d210801ed9c184138be705
Cristian Andrés Villarreal Orozco <147870401+cristianvo74@users.noreply.github.com>
2026-01-19 14:14:30 -0500

    File updated at 19/1/2026, 2:14:30 p. m.

--- 1768-MergeStringsAlternately/1768-MergeStringsAlternately.py
+++ 1768-MergeStringsAlternately/1768-MergeStringsAlternately.py
@@ -1,4 +1,4 @@
-# Last updated: 19/1/2026, 2:11:34 p. m.
+# Last updated: 19/1/2026, 2:14:30 p. m.
 1class Solution:
 2    def mergeAlternately(self, word1: str, word2: str) -> str:
 3        min_l = min(len(word1), len(word2))
@@ -6,8 +6,7 @@
 5        for a,b in zip(word1, word2):
 6            new_w.append(a)
 7            new_w.append(b)
-8        new_w += word1[min_l:].split()
-9        new_w += word2[min_l:].split()
+8        new_w.append(word1[min_l:])
+9        new_w.append(word2[min_l:])
 10        new_w = "".join(new_w)
-11
-12        return new_w
+11        return new_w
