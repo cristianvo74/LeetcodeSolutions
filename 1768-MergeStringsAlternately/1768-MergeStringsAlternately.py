474407780825d98f82370235769bdb7fecffb83a
Cristian Andrés Villarreal Orozco <147870401+cristianvo74@users.noreply.github.com>
2026-01-19 14:11:05 -0500

    File updated at 19/1/2026, 2:11:04 p. m.

--- 1768-MergeStringsAlternately/1768-MergeStringsAlternately.py
+++ 1768-MergeStringsAlternately/1768-MergeStringsAlternately.py
@@ -1,15 +1,14 @@
-# Last updated: 19/1/2026, 1:42:05 p. m.
+# Last updated: 19/1/2026, 2:11:04 p. m.
 1class Solution:
 2    def mergeAlternately(self, word1: str, word2: str) -> str:
-3        max_lenght = max(len(word1), len(word2))
-4        new_word = ""
-5        for i in range(max_lenght):
-6            if i < len(word1):
-7                a = word1[i]
-8            if i < len(word2):
-9                b = word2[i]
-10            new_word += a
-11            new_word += b
-12            a, b = "", ""
-13        return new_word
-14
+3        min_l = min(len(word1), len(word2))
+4
+5        new_w = []
+6        for a,b in zip(word1, word2):
+7            new_w.append(a)
+8            new_w.append(b)
+9        new_w += word1[min_l:].split()
+10        new_w += word2[min_l:].split()
+11        new_w = "".join(new_w)
+12
+13        return new_w
