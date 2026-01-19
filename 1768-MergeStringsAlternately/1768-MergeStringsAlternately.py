510bf0cd6aea046b10099cb550b2f0d23e3e4973
Cristian Andrés Villarreal Orozco <147870401+cristianvo74@users.noreply.github.com>
2026-01-19 14:18:31 -0500

    File updated at 19/1/2026, 2:18:30 p. m.

--- 1768-MergeStringsAlternately/1768-MergeStringsAlternately.py
+++ 1768-MergeStringsAlternately/1768-MergeStringsAlternately.py
@@ -1,4 +1,4 @@
-# Last updated: 19/1/2026, 2:16:47 p. m.
+# Last updated: 19/1/2026, 2:18:30 p. m.
 1class Solution:
 2    def mergeAlternately(self, word1: str, word2: str) -> str:
 3        min_l = min(len(word1), len(word2))
