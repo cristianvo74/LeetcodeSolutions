a978302cbcc4bc9aea0c59078a89c99482ca8de6
Cristian Andrés Villarreal Orozco <147870401+cristianvo74@users.noreply.github.com>
2026-01-20 02:08:04 -0500

    File updated at 20/1/2026, 2:08:04 a. m.

--- 1071-GreatestCommonDivisorofStrings/1071-GreatestCommonDivisorofStrings.py
+++ 1071-GreatestCommonDivisorofStrings/1071-GreatestCommonDivisorofStrings.py
@@ -1,12 +1,5 @@
-# Last updated: 20/1/2026, 2:05:34 a. m.
+# Last updated: 20/1/2026, 2:08:04 a. m.
 1class Solution:
 2    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
 3        max_val = max(candies)
-4        result = []
-5        for kid in candies:
-6            if kid + extraCandies >= max_val:
-7                result.append(True)
-8            else:
-9                result.append(False)
-10        return result
-11        
+4        return [kid + extraCandies >= max_val for kid in candies]
