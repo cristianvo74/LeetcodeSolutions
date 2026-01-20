8281d7cec1ba412c167934a277e6645dd1c65011
Cristian Andrés Villarreal Orozco <147870401+cristianvo74@users.noreply.github.com>
2026-01-20 01:55:22 -0500

    Create 1071-GreatestCommonDivisorofStrings

---
+++ 1071-GreatestCommonDivisorofStrings/1071-GreatestCommonDivisorofStrings.py
@@ -0,0 +1,9 @@
+# Last updated: 20/1/2026, 1:55:21 a. m.
+1class Solution:
+2    def gcdOfStrings(self, str1: str, str2: str) -> str:
+3        for i in range(len(str1), 0 , -1):
+4            candidate = str1[:i]
+5            if len(str2) % i == 0:
+6                if str2 == candidate * (len(str2)//len(candidate)) and str1 == candidate * (len(str1)//len(candidate)):
+7                    return candidate
+8        return ""
