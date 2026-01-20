ff8d1c0f960cc5c2777d61be5ef0a338cef14f30
Cristian Andrés Villarreal Orozco <147870401+cristianvo74@users.noreply.github.com>
2026-01-20 02:30:47 -0500

    File updated at 20/1/2026, 2:30:46 a. m.

--- 1071-GreatestCommonDivisorofStrings/1071-GreatestCommonDivisorofStrings.py
+++ 1071-GreatestCommonDivisorofStrings/1071-GreatestCommonDivisorofStrings.py
@@ -1,5 +1,21 @@
-# Last updated: 20/1/2026, 2:08:04 a. m.
+# Last updated: 20/1/2026, 2:30:46 a. m.
 1class Solution:
-2    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
-3        max_val = max(candies)
-4        return [kid + extraCandies >= max_val for kid in candies]
+2    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+3        counter = 0
+4
+5        for i in range(len(flowerbed)):
+6            if i == 0 and flowerbed[i] == 0 and len(flowerbed) == 1:
+7                counter += 1
+8            elif i == 0 and flowerbed[i] == 0 and flowerbed[1] == 0:
+9                flowerbed[i] = 1
+10                counter += 1
+11            elif i > 0 and i < (len(flowerbed) - 1) and flowerbed[i] == 0 and flowerbed[i-1] == 0 and flowerbed[i+1] == 0:
+12                flowerbed[i] = 1
+13                counter += 1
+14            elif i == (len(flowerbed) - 1) and flowerbed[i] == 0 and flowerbed[i-1] == 0:
+15                flowerbed[i] = 1
+16                counter += 1
+17        if n <= counter:
+18            return True
+19        else:
+20            return False
