d7a9a06b623dc014f00d36a206af2c90dd941a3f
Cristian Andrés Villarreal Orozco <147870401+cristianvo74@users.noreply.github.com>
2026-01-20 13:15:38 -0500

    Create 151-ReverseWordsinaString

---
+++ 151-ReverseWordsinaString/151-ReverseWordsinaString.py
@@ -0,0 +1,7 @@
+# Last updated: 20/1/2026, 1:15:38 p. m.
+1class Solution:
+2    def reverseWords(self, s: str) -> str:
+3        s = s.split()
+4        s = s[::-1]
+5        s = " ".join(s)
+6        return s
